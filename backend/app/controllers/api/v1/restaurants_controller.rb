e8e4c23ab524b41cddac5e0d214d20cdde918e01
takuma <takuma012011@gmail.com>
2022-01-30 11:01:26 +0900

    Rails API 機能作成

---
+++ backend/app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,15 @@
+# moduleを使用することでRestaurantsControllerはApi/V1フォルダ配下にあることを示している
+module Api
+    module V1
+      class RestaurantsController < ApplicationController
+        def index
+          restaurants = Restaurant.all
+  
+          render json: {
+            restaurants: restaurants
+          }, status: :ok
+        end
+      end
+    end
+  end
+  
